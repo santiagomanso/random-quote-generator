4f5d0e4c2766969baa9a0e661b2cc3c7d8156044
santiagomanso <santiagomanso@web.de>
2022-06-20 12:02:31 +0200

    🚧working on mobil design 🎨 improved UI large-viewPort

--- src/App.js
+++ src/App.js
@@ -3,20 +3,35 @@ import './styles.css'
 function App() {
   return (
     <div id='quote-box'>
-      <div className='wrapper'>
-        <div id='text'>sdlkjdfglksdjjfgkldgkjdfggfdfs</div>
-        <div id='author'>- Napoleon</div>
-        <div className='buttons'>
-          <div className='flex'>
-            <div className='icon'>
-              <i class='fa-brands fa-twitter'></i>
-            </div>
-            <div>
-              <i class='fa-brands fa-tumblr'></i>
-            </div>
+      <div className='p-2'>
+        <div className='wrapper'>
+          <div id='text' className='text-div'>
+            <p>
+              "If you’re offered a seat on a rocket ship, don’t ask what seat!
+              Just get on.""
+            </p>
           </div>
-          <button id='new-quote'>new quote</button>
+          <div id='author'>
+            <div></div>
+            <p>- napoleon</p>
+          </div>
+          <div className='buttons'>
+            <div className='wrapper-icons'>
+              <div className='icon cursor-pointer'>
+                <i className='fa-brands fa-twitter'></i>
+              </div>
+              <div className='icon cursor-pointer'>
+                <i className='fa-brands fa-tumblr'></i>
+              </div>
+            </div>
+
+            <button id='new-quote' className='cursor-pointer'>
+              new quote
+            </button>
+          </div>
         </div>
+
+        <p className='santi'>By Santiago Manso Castro</p>
       </div>
     </div>
   )
